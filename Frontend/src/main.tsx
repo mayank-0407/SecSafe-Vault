c4cef748e0bb712e7bbc0d0e66f65c806c2d27b6
Mayank Aggarwal <mayankdatabase04@gmail.com>
2025-08-09 14:21:09 +0530

    Added vercel Analytics

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { PasswordVaultProvider } from "./Utils/context/PasswordVaultContext.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PasswordVaultProvider>
+      <Analytics />
       <App />
     </PasswordVaultProvider>
   </StrictMode>
